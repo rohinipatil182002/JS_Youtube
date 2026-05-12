eee527f55f818b2f9134920f608d39d05d913861
Rohini Patil <rohinipatil182002@gmail.com>
2026-05-12 19:25:41 +0530

    String basics

--- 01_Basics/datatype-summary.js
+++ 01_Basics/datatype-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -32,6 +32,23 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof heros);
+//console.log(typeof heros);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+let name="Rohini"
+let anotherName = name
+
+anotherName = "Hitesh"
+console.log(name);
+console.log(anotherName);
+
+let myHero = {
+    name: "Shaktiman",
+    power: "All"
+}
+let anotherHero = myHero
+
+anotherHero.power = "Some"
+console.log(myHero);
+console.log(anotherHero);
