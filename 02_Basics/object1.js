6f75c6a28e48e89471bf69e0750eb90b64e65574
Rohini Patil <rohinipatil182002@gmail.com>
2026-05-14 11:02:31 +0530

    Its all about objects Related

---
+++ 02_Basics/object1.js
@@ -0,0 +1,39 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+const mysym2=Symbol("MyName")
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    [mysym2]:"Rohini Patil",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
+console.log(JsUser["full name"])
+console.log(JsUser[mySym])
+console.log(JsUser[mysym2])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+ console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
